6b803cf8bdde92e2581eba2efa8e4d263a99c991
Daragh Farrell <daragh.farrell@gmail.com>
2016-12-13 11:48:04 +0000

    working on data, prob with idea

---
+++ src/main/java/com/gw/apex/hack/yabe/TestDomainOne.java
@@ -0,0 +1,11 @@
+package com.gw.apex.hack.yabe;
+
+import org.springframework.data.gemfire.mapping.Region;
+
+/**
+ * @author dfarrell on 13/12/2016.
+ */
+
+@Region
+public class TestDomainOne {
+}

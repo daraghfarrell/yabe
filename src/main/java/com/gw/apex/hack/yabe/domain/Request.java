f3d29655a499e71435686cc39ea18ed8e8afc7a8
Will Manning <wmanning@guidewire.com>
2017-01-12 14:01:47 +0000

    First commit of Request classes and repo

---
+++ src/main/java/com/gw/apex/hack/yabe/domain/Request.java
@@ -0,0 +1,7 @@
+package com.gw.apex.hack.yabe.domain;
+
+/**
+ * Created by wmanning on 12/01/2017.
+ */
+public class Request {
+}
---
+++ src/main/java/com/gw/apex/hack/yabe/repo/RequestToBuyRepo.java
@@ -0,0 +1,7 @@
+package com.gw.apex.hack.yabe.repo;
+
+/**
+ * Created by wmanning on 12/01/2017.
+ */
+public class RequestToBuyRepo {
+}
